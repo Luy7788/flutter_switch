f5e643a2938b5ee41e11445d61c06c3074f82a43
luy <luy7788@163.com>
2020-11-28 02:39:56 +0800

    no message

--- flutter_switch/lib/switch.dart
+++ flutter_switch/lib/switch.dart
@@ -57,90 +57,98 @@ class _SwitchButtonState extends State<SwitchButton> {
     setState(() {});
   }
 
+  backgroundWidget() {
+    return GestureDetector(
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: widget.unSelectColor ?? Colors.grey[200],
+          borderRadius: BorderRadius.circular(widget.height / 2),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              widget.tabs[0] ?? "",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              widget.tabs[1] ?? "",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        int currentIndex;
+        if (widget.currentIndex == 0) {
+          currentIndex = 1;
+        } else {
+          currentIndex = 0;
+        }
+        changePage(currentIndex);
+      },
+    );
+  }
+
+  switchWidget() {
+    return AnimatedPositioned(
+      left: widget.currentIndex == 0 ? 0 : this.centerPoint - _padding,
+      duration: _duration,
+      child: GestureDetector(
+        onHorizontalDragUpdate: (DragUpdateDetails value) {
+          _dragDistance = value.localPosition.dx;
+        },
+        onHorizontalDragEnd: (DragEndDetails value) {
+          if (_dragDistance < -10) {
+            changePage(0);
+          } else if (_dragDistance > 10) {
+            changePage(1);
+          }
+        },
+        onHorizontalDragStart: (value) {
+          _dragDistance = 0;
+        },
+        child: Container(
+          width: centerPoint - _padding,
+          height: widget.height - _padding * 2,
+          decoration: BoxDecoration(
+            color: widget.selectColor ?? Colors.white,
+            borderRadius: BorderRadius.circular(
+              (widget.height - _padding * 2) / 2,
+            ),
+          ),
+          margin: EdgeInsets.all(_padding),
+          child: Center(
+            child: Text(
+              widget.tabs[widget.currentIndex] ?? "",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        GestureDetector(
-          child: Container(
-            width: widget.width,
-            height: widget.height,
-            decoration: BoxDecoration(
-              color: widget.unSelectColor ?? Colors.grey[200],
-              borderRadius: BorderRadius.circular(widget.height / 2),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  widget.tabs[0] ?? "",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  widget.tabs[1] ?? "",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          onTap: () {
-            int currentIndex;
-            if (widget.currentIndex == 0) {
-              currentIndex = 1;
-            } else {
-              currentIndex = 0;
-            }
-            changePage(currentIndex);
-          },
-        ),
-        AnimatedPositioned(
-          left: widget.currentIndex == 0 ? 0 : this.centerPoint - _padding,
-          duration: _duration,
-          child: GestureDetector(
-            onHorizontalDragUpdate: (DragUpdateDetails value) {
-              _dragDistance = value.localPosition.dx;
-            },
-            onHorizontalDragEnd: (DragEndDetails value) {
-              if (_dragDistance < -10) {
-                changePage(0);
-              } else if (_dragDistance > 10) {
-                changePage(1);
-              }
-            },
-            onHorizontalDragStart: (value) {
-              _dragDistance = 0;
-            },
-            child: Container(
-              width: centerPoint - _padding,
-              height: widget.height - _padding * 2,
-              decoration: BoxDecoration(
-                color: widget.selectColor ?? Colors.white,
-                borderRadius: BorderRadius.circular(
-                  (widget.height - _padding * 2) / 2,
-                ),
-              ),
-              margin: EdgeInsets.all(_padding),
-              child: Center(
-                child: Text(
-                  widget.tabs[widget.currentIndex] ?? "",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        backgroundWidget(),
+        switchWidget(),
       ],
     );
   }
